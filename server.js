fd63ae30414a98d7a6e19584fc2d40cc5d0f32a2
anUnknownStoic <104714642+benbiji63@users.noreply.github.com>
2023-08-18 19:03:09 +0530

    Built basics structure Added mongo db

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(3000, function () {
-  console.log('Listening to 3000');
-});
-app.get('/', (req, res) => {
-  // res.send('Hello World')
-  res.sendFile(__dirname + '/index.html');
-});
-app.post('/quotes', (req, res) => {
-  console.log(req.body);
-});
+const MongoClient = require('mongodb').MongoClient;
+const connectionString =
+  'mongodb+srv://infinity:246Ilok)&@cluster0.6a430ku.mongodb.net/?retryWrites=true&w=majority';
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then( client => {
+    console.log('connected to db');
+    const db = client.db('star-wars-quotes');
+    const quotesCollection = db.collection('quotes');
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.listen(3000, function () {
+      console.log('Listening to 3000');""
+    });
+    app.get('/', (req, res) => {
+      // res.send('Hello World')
+      res.sendFile(__dirname + '/index.html');
+    });
+    app.post('/quotes', (req, res) => {
+      console.log(req.body);
+    });""
+  })
+  .catch(err => console.error(err));
